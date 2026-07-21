1733a108be3fe71937045d62290d1dc9a5a3b20a
Midnite <151900351+Midnite10@users.noreply.github.com>
2026-07-22 10:15:08 +1200

    Made functions to reset deck and hand
    Specific functions to reset (clear) either the hand, the deck, or both, instead of automatically resetting the deck only when user starts a new game (build_deck())

--- cards.py
+++ cards.py
@@ -3,16 +3,16 @@ import random as rand
 import os
 
 class PlayingCards:
-
-    def __init__(self, card_quantity):
+    """Class for any card game. Creates, shuffles, and deals cards to the player."""
+    def __init__(self):
         self.suits = ["♠", "♥", "♦", "♣"]
         self.values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
         self.deck = [] # Full deck of cards that the game draws from
         self.hand = [] # Players hand that they play with
-        self.card_quantity = card_quantity # The number of cards that the player recieves
     
 
     def build_deck(self):
+        # Creates a new deck at the start of each game. Make sure to clear the deck and hand first.
         for suit in self.suits:
             for value in self.values:
                 self.deck.append((suit, value))
@@ -23,6 +23,7 @@ class PlayingCards:
 
 
     def shuffle_deck(self):
+        # Randomises the order of the cards before dealing
         rand.shuffle(self.deck)
 
         print("- After Shuffling - ")
@@ -30,8 +31,10 @@ class PlayingCards:
         print("hand:", self.hand)
     
 
-    def deal_cards(self):
-        for i in range(self.card_quantity):
+    def deal_cards(self, card_quantity):
+        # Gives a certain amount of cards to the player
+        # Put an integer as the argument to deal that many cards
+        for i in range(card_quantity):
             card = self.deck.pop(0)
             self.hand.append(card)
 
@@ -40,9 +43,29 @@ class PlayingCards:
         print("hand:", self.hand)
 
 
+    def clear_deck(self):
+        self.deck.clear() # Resets deck for the new game
+    
+
+    def clear_hand(self):
+        self.hand.clear() # Wipes hand for the new game
+    
+
+    def reset_cards(self):
+        self.clear_deck()
+        self.clear_hand()
+        print("deck:", self.deck)
+        print("hand:", self.hand)
+    
+
+
 # TESTING
 os.system('cls' if os.name == 'nt' else 'clear')
-testcards = PlayingCards(2)
+testcards = PlayingCards()
 testcards.build_deck()
 testcards.shuffle_deck()
-testcards.deal_cards()
+testcards.deal_cards(2)
+testcards.deal_cards(1)
+testcards.clear_deck()
+testcards.build_deck()
+testcards.reset_cards()
