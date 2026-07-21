72b2875fb9a82dd3e798902aaf77ac29255ba326
Midnite <151900351+Midnite10@users.noreply.github.com>
2026-07-21 15:20:04 +1200

    Added deck creation to the class.
    Decks can now be created on demand by the build_deck() function. Lists for suits, values, deck, and hand have been moved into the __init__ of the playing cards class.

--- cards.py
+++ cards.py
@@ -2,30 +2,32 @@ import tkinter as tk
 import random as rand
 import os
 
-
-suits = ["♠", "♥", "♦", "♣"]
-values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
-deck = []
-hand = []
-
 class PlayingCards:
 
-    def __init__(self, deck, hand, card_quantity):
-        self.suits = suits 
-        self.values = values
-        self.deck = deck # Full deck of cards that the game draws from
-        self.hand = hand # Players hand that they play with
+    def __init__(self, card_quantity):
+        self.suits = ["♠", "♥", "♦", "♣"]
+        self.values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+        self.deck = [] # Full deck of cards that the game draws from
+        self.hand = [] # Players hand that they play with
         self.card_quantity = card_quantity # The number of cards that the player recieves
     
 
     def build_deck(self):
-        for suit in suits:
-            for value in values:
-                deck.append((suit, value))
+        for suit in self.suits:
+            for value in self.values:
+                self.deck.append((suit, value))
+
+        print("- Before Shuffling & Dealing - ")
+        print("deck:", self.deck)
+        print("hand:", self.hand)
 
 
     def shuffle_deck(self):
         rand.shuffle(self.deck)
+
+        print("- After Shuffling - ")
+        print("deck:", self.deck)
+        print("hand:", self.hand)
     
 
     def deal_cards(self):
@@ -33,20 +35,14 @@ class PlayingCards:
             card = self.deck.pop(0)
             self.hand.append(card)
 
+        print("- After Dealing - ")
+        print("deck:", self.deck)
+        print("hand:", self.hand)
+
 
 # TESTING
 os.system('cls' if os.name == 'nt' else 'clear')
-testcards = PlayingCards(deck, hand, 2)
+testcards = PlayingCards(2)
 testcards.build_deck()
-print("- Before Shuffling & Dealing - ")
-print("deck:", deck)
-print("hand:", hand)
-print() # gap
 testcards.shuffle_deck()
-print("- After Shuffling - ")
-print("deck:", deck)
-print() # gap
 testcards.deal_cards()
-print("- After Dealing - ")
-print("deck:", deck)
-print("hand:", hand)
