ef0e7b8f7474dfc2bf1fb7ccdc6e5cb038974302
Midnite <151900351+Midnite10@users.noreply.github.com>
2026-07-23 09:12:52 +1200

    Card dealing system returns cards to global lists
    Instead of storing hands in the PlayingCards() class, I made it so that the cards are returned to a list.
    
    Example of code being used in blackjack:
    player_hand.extend(blackjack.deal_cards(2))

--- cards.py
+++ cards.py
@@ -1,4 +1,3 @@
-import tkinter as tk 
 import random as rand
 import os
 
@@ -8,7 +7,6 @@ class PlayingCards:
         self.suits = ["♠", "♥", "♦", "♣"]
         self.values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
         self.deck = [] # Full deck of cards that the game draws from
-        self.hand = [] # Players hand that they play with
     
 
     def build_deck(self):
@@ -17,55 +15,56 @@ class PlayingCards:
             for value in self.values:
                 self.deck.append((suit, value))
 
-        print("- Before Shuffling & Dealing - ")
-        print("deck:", self.deck)
-        print("hand:", self.hand)
 
 
     def shuffle_deck(self):
         # Randomises the order of the cards before dealing
         rand.shuffle(self.deck)
-
-        print("- After Shuffling - ")
-        print("deck:", self.deck)
-        print("hand:", self.hand)
     
 
     def deal_cards(self, card_quantity):
-        # Gives a certain amount of cards to the player
+        # Deals a certain amount of cards and returns them as a list.
         # Put an integer as the argument to deal that many cards
-        for i in range(card_quantity):
-            card = self.deck.pop(0)
-            self.hand.append(card)
+        # In the game code, type: [hand var].extend([game var].deal_cards([num]))
+        dealt = [] # stores the cards that are going to be given.
+        if self.cards_remaining() < card_quantity:
 
-        print("- After Dealing - ")
-        print("deck:", self.deck)
-        print("hand:", self.hand)
+            # Makes sure there is cards remaining in the deck
+            raise ValueError("Not enough cards left in the deck!")
+        
+        for i in range(card_quantity):
+            dealt.append(self.deck.pop())
+        
+        return dealt
 
 
     def clear_deck(self):
         self.deck.clear() # Resets deck for the new game
     
 
-    def clear_hand(self):
-        self.hand.clear() # Wipes hand for the new game
-    
-
     def reset_cards(self):
         self.clear_deck()
         self.clear_hand()
-        print("deck:", self.deck)
-        print("hand:", self.hand)
+
+
+    def cards_remaining(self):
+        return len(self.deck)
     
 
 
 # TESTING
 os.system('cls' if os.name == 'nt' else 'clear')
-testcards = PlayingCards()
-testcards.build_deck()
-testcards.shuffle_deck()
-testcards.deal_cards(2)
-testcards.deal_cards(1)
-testcards.clear_deck()
-testcards.build_deck()
-testcards.reset_cards()
+blackjack = PlayingCards()
+blackjack.build_deck()
+blackjack.shuffle_deck()
+
+player_hand = []
+dealer_hand = []
+
+player_hand.extend(blackjack.deal_cards(2))
+dealer_hand.extend(blackjack.deal_cards(2))
+
+print(player_hand)
+print(dealer_hand)
+
+
