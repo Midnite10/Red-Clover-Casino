41a65b608d55e2457eab5b4c9478ddffd5bc51b1
Midnite <151900351+Midnite10@users.noreply.github.com>
2026-07-29 10:32:44 +1200

    Finished homepage button layout
    Button size issue still persistent

--- gui.py
+++ gui.py
@@ -7,7 +7,11 @@ class CasinoGUI:
         self.root.title("Red Clover Casino")
         self.root.geometry("800x600")
 
-        # Home page
+        self.homepage()
+
+
+    def show_homepage(self):
+        # ----- HOME PAGE    -----
 
         self.homepage = tk.Frame(self.root, bg="#1f1b1d")
         self.homepage.pack(fill="both", expand=True)
@@ -16,7 +20,7 @@ class CasinoGUI:
         self.homepage.rowconfigure([1, 2, 3, 4], weight=3)
         self.homepage.columnconfigure([0, 1, 2, 3], weight=1)
 
-        # Header frame
+        # Header frame (occupies the top row)
 
         self.header_frame = tk.Frame(self.homepage, bg="#d4142a")
         self.header_frame.grid(row=0, column=0, columnspan=4, sticky="nsew")
@@ -36,20 +40,28 @@ class CasinoGUI:
 
         # Button screen
 
-        self.blackjack_button = tk.Button(self.homepage, text="BLACKJACK", bg="#d4142a", fg="black", font="Arial 22 bold")
-        self.blackjack_button.grid(row=1, column=0, columnspan=2, rowspan=2, ipadx=(32, 16), ipady=(32, 16), sticky="nsew")
+        self.blackjack_button = tk.Button(self.homepage, text="BLACKJACK", bg="#d4142a", fg="black", font="Arial 22 bold", borderwidth=0)
+        self.blackjack_button.grid(row=1, column=0, columnspan=2, rowspan=2, padx=(32, 16), pady=(32, 16), sticky="nsew")
 
-        self.slots_button = tk.Button(self.homepage, text="SLOTS", bg="#d4142a", fg="black", font="Arial 22 bold")
-        self.slots_button.grid(row=1, column=2, columnspan=2, rowspan=2, ipadx=(16, 32), ipady=(32, 16), sticky="nsew")
+        self.slots_button = tk.Button(self.homepage, text="SLOTS", bg="#d4142a", fg="black", font="Arial 22 bold", borderwidth=0)
+        self.slots_button.grid(row=1, column=2, columnspan=2, rowspan=2, padx=(16, 32), pady=(32, 16), sticky="nsew")
 
-        self.highlow_button = tk.Button(self.homepage, text="HIGH/LOW", bg="#d4142a", fg="black", font="Arial 22 bold")
-        self.highlow_button.grid(row=3, column=0, columnspan=2, rowspan=2, ipadx=(32, 16), ipady=(16, 32), sticky="nsew")
+        self.highlow_button = tk.Button(self.homepage, text="HIGH/LOW", bg="#d4142a", fg="black", font="Arial 22 bold", borderwidth=0)
+        self.highlow_button.grid(row=3, column=0, columnspan=2, rowspan=2, padx=(32, 16), pady=(16, 32), sticky="nsew")
 
-        self.leaderboard_button = tk.Button(self.homepage, text="LEADERBOARD", bg="#d4142a", fg="black", font="Arial 22 bold")
-        self.leaderboard_button.grid(row=3, column=2, columnspan=2, ipadx=(16, 32), ipady=16, sticky="nsew")
+        # Profile/Leaderboard container
 
-        self.profile_button = tk.Button(self.homepage, text="PROFILE", bg="#d4142a", fg="black", font="Arial 22 bold")
-        self.profile_button.grid(row=4, column=2, columnspan=2, ipadx=(16, 32), ipady=(16, 32), sticky="nsew")
+        self.info_frame = tk.Frame(self.homepage, bg="#1f1b1d")
+        self.info_frame.grid(row= 3, column=2, columnspan=2, rowspan=2, padx=(16, 32), pady=(16, 32), sticky="nsew")
+
+        self.info_frame.rowconfigure([0, 1], weight=1)
+        self.info_frame.columnconfigure(0, weight=1)
+
+        self.leaderboard_button = tk.Button(self.info_frame, text="LEADERBOARD", bg="#d4142a", fg="black", font="Arial 22 bold", borderwidth=0)
+        self.leaderboard_button.grid(row=0, column=0, pady=(0, 16), sticky="nsew")
+
+        self.profile_button = tk.Button(self.info_frame, text="PROFILE", bg="#d4142a", fg="black", font="Arial 22 bold", borderwidth=0)
+        self.profile_button.grid(row=1, column=0, pady=(16, 0), sticky="nsew")
 
 
 # Main program Function
