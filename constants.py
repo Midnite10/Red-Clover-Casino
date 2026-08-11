0e726589e08885c9ec6d438551d6800f632cbe53
Midnite <151900351+Midnite10@users.noreply.github.com>
2026-08-11 15:03:10 +1200

    Casino Constants

---
+++ constants.py
@@ -0,0 +1,26 @@
+class Constants:
+    # ----- General -----
+    CASINO_NAME = "Red Clover Casino"
+    WINDOW_SIZE = "800x600"
+
+    # ----- Colours -----
+    BG_COLOUR = "#1f1b1d" # Background colour
+    MAIN_COLOUR = "#d4142a" # Main red colour
+    TABLE_COLOUR = "#008000" # For blackjack table
+    CARD_BACK_COLOUR = "#9e0000" # The back of the card
+    CARD_FRONT_COLOUR = "white" # The front of the card
+    WHITE = "white" # can change to a different shade of white if needed
+
+    # ----- Fonts -----
+    TITLE_FONT = "Arial 28 bold"
+    LG_FONT = "Arial 22 bold"
+    MD_FONT = "Arial 18 bold"
+    SM_FONT = "Arial 14"
+    SM_FONT_BOLD = "Arial 14 bold"
+
+    # ----- Accounts -----
+    STARTING_BALANCE = 1000
+    MIN_USERNAME_LENGTH = 3
+    MAX_USERNAME_LENGTH = 16
+    MIN_PASSWORD_LENGTH = 8
+    MAX_PASSWORD_LENGTH = 32
